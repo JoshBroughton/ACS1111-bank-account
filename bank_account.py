5eca75abfc86f8e0a7831a1c097338e598a79943
Josh Broughton <josh.broughton47@gmail.com>
2022-10-25 14:10:50 -0600

    completed print_statement function and a helper function for getting the redacted account number

--- bank_account.py
+++ bank_account.py
@@ -22,7 +22,7 @@ class BankAccount:
 
     def deposit(self, amount):
         """adds amount to the balance of the account, and prints a confirmation message"""
-        if type(amount) is int or type(amount) is float:
+        if isinstance(amount, (float, int)):
             self.balance += amount
             print(f'Amount deposited: ${amount} New Balance: ${self.balance}')
         else:
@@ -31,7 +31,7 @@ class BankAccount:
     def withdraw(self, amount):
         """subtracts amount from the balance of the account. Does not allow withdrawal to negatives,
         and charges a $10 overdraft fee if an attempt is made to do so."""
-        if type(amount) is int or type(amount) is float:
+        if isinstance(amount, (float, int)):
             balance = self.balance - amount
             if balance > 0:
                 self.balance = balance
@@ -43,6 +43,18 @@ class BankAccount:
         
     def get_balance(self):
         """prints and returns the current balance of the account"""
+        print(f'The current balance of this account is {self.balance}')
+        return self.balance
 
     def print_statement(self):
         """prints a statement message with account name, number, and balance"""
+        statement_string = f'{self.full_name}\nAccount No.: {self.get_redacted_account_number()}\nBalance: ${self.balance}'
+        print(statement_string)
+
+    def get_redacted_account_number(self):
+        """returns the account number with the first four digits redacted and replaced with ****,
+        e.g. 12345678 -> ****5678"""
+        account_number = self.account_number
+        for i in range(4):
+            account_number[i] = '*'
+        return account_number
