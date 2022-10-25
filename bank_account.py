9be3ee55e9fe39a796e2ff670e39de235a6102fc
Josh Broughton <josh.broughton47@gmail.com>
2022-10-25 15:58:30 -0600

    added currency rounding to decimal points

--- bank_account.py
+++ bank_account.py
@@ -1,16 +1,16 @@
 from random import randint
-from tkinter import MITER
 
 class BankAccount:
-    """Defines a BankAccount class, which represents a generic bank account. Includes 
-    attributes for full name of account holder, account number, and balance. Includes 
-    methods to generate a random account number, assign a manual account number, 
+    """Defines a BankAccount class, which represents a generic bank account. Includes
+    attributes for full name of account holder, account number, and balance. Includes
+    methods to generate a random account number, assign a manual account number,
     deposit, withdraw, and view a statement of account."""
     def __init__(self, full_name, account_numbers, overdraft_fee=10):
         self.full_name = full_name
         self.account_number = self.generate_account_number(account_numbers)
         self.balance = 0
         self.overdraft_fee = overdraft_fee
+        self.interest_rate = 1
 
     def generate_account_number(self, account_numbers):
         """Generates and assigns a random 8-digit account number to the account.
@@ -22,6 +22,7 @@ class BankAccount:
             new_account_number = str(new_account_number).rjust(8, '0')
             if new_account_number in account_numbers:
                 unique_account_number = False
+                print('Duplicate found!')
             else:
                 unique_account_number = True
                 return new_account_number
@@ -33,7 +34,7 @@ class BankAccount:
     def deposit(self, amount):
         """adds amount to the balance of the account, and prints a confirmation message"""
         if isinstance(amount, (float, int)):
-            self.balance += amount
+            self.balance += round(amount, 2)
             print(f'Amount deposited: ${amount} New Balance: ${self.balance}')
         else:
             print('Please enter a valid numeric entry for the amount to deposit.')
@@ -42,7 +43,7 @@ class BankAccount:
         """subtracts amount from the balance of the account. Does not allow withdrawal to negatives,
         and charges a $10 overdraft fee if an attempt is made to do so."""
         if isinstance(amount, (float, int)):
-            balance = self.balance - amount
+            balance = self.balance - round(amount, 2)
             if balance > 0:
                 self.balance = balance
                 print(f'Amount withdrawn: ${amount} New balance: ${balance}')
@@ -58,9 +59,10 @@ class BankAccount:
     def add_interest(self):
         """adds interest at a rate of 1% annually, compounded monthly (0.083% per month). 
         This function adds one month worth of interest"""
-        interest = self.balance * 0.0083
+        interest = self.balance * (self.interest_rate / 100 / 12)
+        interest = round(interest, 2)
         self.balance += interest
-        print(f'Added interest. New account balance is {self.balance}')
+        print(f'Added interest. New account balance is ${self.balance}')
 
     def print_statement(self):
         """prints a statement message with account name, number, and balance"""
@@ -78,15 +80,12 @@ class BankAccount:
         account_number = '****' + account_number[4:8]
         return account_number
 
-account_numbers = []
-josh_account = BankAccount('Josh Broughton', account_numbers)
-account_numbers.append(josh_account.get_account_number())
-mitchell_account = BankAccount('Mitchell', account_numbers)
+accounts= []
+josh_account = BankAccount('Josh Broughton', accounts)
+accounts.append(josh_account.get_account_number())
+mitchell_account = BankAccount('Mitchell', accounts)
 mitchell_account.assign_account_number('03141592')
-josh_account.deposit(400)
-josh_account.withdraw(200)
-josh_account.withdraw(300)
-josh_account.get_balance()
+josh_account.deposit(1000.555555555)
 josh_account.add_interest()
-josh_account.print_statement()
-mitchell_account.print_statement()
+
+
